aa937ab203b040d5e2371108cf8c3fe63d47005e
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2020-12-18 12:04:24 +0100

    the cli can now list zones and records

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -7,34 +7,46 @@ use std::fs;
     version = "1.0",
     author = "Guillaume A. <25181283+Gui-Yom@users.noreply.github.com>"
 )]
-struct Opts {
-    #[clap(subcommand)]
-    subcmd: SubCommand,
+enum Opts {
+    Zones {
+        #[clap(subcommand)]
+        subcmd: ZonesSubCommand,
+    },
+    Records {
+        zone_id: u64,
+        #[clap(subcommand)]
+        subcmd: RecordsSubCommand,
+    },
 }
 
 #[derive(Clap)]
-enum SubCommand {
-    Test,
-    Print,
+enum ZonesSubCommand {
+    List,
+}
+
+#[derive(Clap)]
+enum RecordsSubCommand {
+    List,
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-    match opts.subcmd {
-        SubCommand::Test => {
-            println!("Test !")
-        }
-        SubCommand::Print => {
-            println!("Print ! ")
-        }
-    }
-
     let token = fs::read_to_string("token.txt").expect("Can't read file token.txt");
     let api = DynV6::new(&token);
-    api.list_zones().iter().for_each(|it| {
-        println!("{}: {} -> {}", it.id, it.name, it.ipv4address);
-        api.list_records(it.id)
-            .iter()
-            .for_each(|r| println!("record: {} {}: {}", r.record_type, r.name, r.data))
-    });
+    match opts {
+        Opts::Zones { subcmd } => match subcmd {
+            ZonesSubCommand::List => {
+                api.list_zones().iter().for_each(|it| {
+                    println!("{}: {} -> {}", it.id, it.name, it.ipv4address);
+                });
+            }
+        },
+        Opts::Records { zone_id, subcmd } => match subcmd {
+            RecordsSubCommand::List => {
+                api.list_records(zone_id)
+                    .iter()
+                    .for_each(|r| println!("record: {} {}: {}", r.record_type, r.name, r.data));
+            }
+        },
+    }
 }
